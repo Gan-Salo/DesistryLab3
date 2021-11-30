22a21388b39723afcc5993b8aadd4cbc7cb25051
Gan-Salo <90299377+Gan-Salo@users.noreply.github.com>
2021-11-30 20:44:34 +0700

    Лабораторная №7

--- DesistryLab3/DesistryLab3.cpp
+++ DesistryLab3/DesistryLab3.cpp
@@ -11,46 +11,6 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    //Cabinet test_cab1, test_cab2;
-    //test_cab2.vvod();
-    //test_cab1.print();
-    //test_cab2.print();
-    //
-    //Cabinet test_cab3[4];
-    //for (int i = 0; i < 4; i++)
-    //{
-    //    test_cab3[i].print();
-    //}
-    //
-    //int n = 3;
-    //Cabinet* test_cab4; //Переменная указатель
-    //test_cab4 = new Cabinet[n]; //Новый тип 
-    //for (int i = 0; i < n; i++)
-    //{
-    //    test_cab4[i].print();
-    //}
-    //
-    //delete[] test_cab4;    //Освобождается память, выделенная оператором 'new'
-   
-    //Patient pat1[3];
-    //for (int i = 0; i < n; i++)
-    //{          
-    //   pat1[i].print();
-    //}
-
-    //Usluga* test_usl = new Usluga;
-    //test_usl->print();  
-    //(*test_usl).print();
-    //test_usl[0].print();
-
-    /*Тестирование функций изменения*/
-
-    /*Zapis test_zap1;
-    test_zap1.vvod();
-    test_zap1.print();
-    test_zap1.change_doc();
-    test_zap1.print();*/
-  
     /*Создаём 'Должность', существующую независимо от класса 'Доктор'*/
     Dolznost test_dolz("Врач-Хирург", "Мед. персонал");  
     //Dolznost *dolzn2 = new Dolznost("Врач-Х", "Мед.");
